e9b0aeacd2836832c90aa0ef05d3a8e2e4199c92
Aitor <aitorpeto2002@gmail.com>
2023-02-23 19:52:29 +0100

    sucio

--- sucio.js
+++ sucio.js
@@ -1,37 +1,55 @@
+// Obtener el elemento de entrada de búsqueda
+const busquedaInput = document.getElementById('busqueda');
+// Obtener el elemento de opciones dinámicas
+const opcionesDiv = document.getElementById('opciones');
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    
-    var xmlDoc = this.responseXML;
+// Función que se ejecuta cada vez que se modifica el valor de búsqueda
+busquedaInput.addEventListener('input', function() {
+  // Obtener el valor de búsqueda
+  const busqueda = busquedaInput.value;
+  // Enviar una solicitud AJAX para obtener los productos que coinciden con la búsqueda
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      // Procesar la respuesta y actualizar la lista de opciones
+      const productos = parseProductosXML(xhr.responseXML);
+      actualizarOpciones(productos);
+    }
+  };
+  xhr.open('GET', `productos.xml`);
+  xhr.send();
+});
 
-    var products = xmlDoc.getElementsByTagName("producto");
-    
-    var selectElement = document.getElementById("productSelect");
+// Función que procesa la respuesta XML y retorna un array de objetos producto
+function parseProductosXML(xmlDoc) {
+  const productos = [];
+  const productosXML = xmlDoc.getElementsByTagName('producto');
+  for (const productoXML of productosXML) {
+    const producto = {
+      codigo: productoXML.getAttribute('codigo'),
+      descripcion: productoXML.getElementsByTagName('descripcion')[0].textContent,
+    };
+    productos.push(producto);
+  }
+  return productos;
+}
 
-    selectElement.innerHTML = "";
-
-    for (var i = 0; i < products.length; i++) {
-      var productId = products[i].getAttribute("id");
-      var productDescription = products[i].getElementsByTagName("descripcion")[0].childNodes[0].nodeValue;
-      var optionElement = document.createElement("option");
-      optionElement.value = productId;
-      optionElement.textContent = productId + " - " + productDescription;
-      selectElement.appendChild(optionElement);
+// Función que actualiza la lista de opciones con los productos obtenidos de la respuesta AJAX
+function actualizarOpciones(productos) {
+  // Limpiar la lista de opciones
+  opcionesDiv.innerHTML = '';
+  // Crear un elemento de lista para cada producto que coincida con la búsqueda
+  for (const producto of productos) {
+    if (producto.codigo.toLowerCase().includes(busquedaInput.value.toLowerCase()) ||
+        producto.descripcion.toLowerCase().includes(busquedaInput.value.toLowerCase())) {
+      const li = document.createElement('li');
+      li.innerText = `${producto.codigo} - ${producto.descripcion}`;
+      // Agregar un evento de clic que seleccione el producto y lo muestre en el campo de búsqueda
+      li.addEventListener('click', function() {
+        busquedaInput.value = `${producto.codigo} - ${producto.descripcion}`;
+        opcionesDiv.innerHTML = '';
+      });
+      opcionesDiv.appendChild(li);
     }
   }
-};
-xhttp.open("GET", "productos.xml", true);
-xhttp.send();
-
-var productSelectElement = document.getElementById("productSelect");
-productSelectElement.addEventListener("change", function() {
-  var productId = this.value;
-  var quantityElement = document.getElementById("quantityInput");
-  quantityElement.value = "";
-  if (productId) {
-    quantityElement.disabled = false;
-  } else {
-    quantityElement.disabled = true;
-  }
-});
+}
